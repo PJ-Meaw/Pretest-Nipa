93f1a8734a577f542f962cd6b456f21fc4da2435
PJ-Meaw <pathinya19@gmail.com>
2025-02-10 15:06:12 +0700

    Change config

--- fastAPI/config.py
+++ fastAPI/config.py
@@ -2,7 +2,7 @@
 DATABASE_CONFIG = {
     "host": "aws-0-ap-southeast-1.pooler.supabase.com",
     "port": 5432,
-    "user": "postgres.uoafavdzmwsybxnfkmhg",
-    "password": "LJIdc4LWeKQOMIpx",
+    "user": "postgres.vmdyjiqnuuyewgbxarfz",
+    "password": "g81al8eaEXWykPh5",
     "database": "postgres",
 }
